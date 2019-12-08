c0d23752529e1284ad88db16d2ae29b4c501648c
mahesdwivedi <mahesdwivedi@gmail.com>
2019-12-08 08:21:18 +0530

    add homepage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,11 +12,28 @@ import CollectorDashboard from "../CollectorDashboard";
 import CollectionList from "../CollectionList";
 import OrderRoute from "../OrderRoute";
 
+import "./App.css";
+
 const App = () => (
   <Router>
     <div>
       <Navigation />
       <hr />
+      <img className="bg" src="./bg.png"></img>
+      <center>
+        <div class="heading">ABOUT US</div>
+        <div>
+          {" "}
+          A vision to curb food waste generated from different sources such as
+          restaurants, airlines, parties,functions,etc. by proper and efficient
+          management of food. Global food loss and waste amount to between
+          one-third and one-half of all food produced. Loss and wastage occur at
+          all stages of the food supply chain or value chain. In low-income
+          countries, most loss occurs during production, while in developed
+          countries much food – about 100 kilograms (220 lb) per person per year
+          – is wasted at the consumption stage{" "}
+        </div>
+      </center>
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
